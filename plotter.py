67ed286592ae8c220e42815802eb7874adeb8e4d
AHF <47339067+Fahad021@users.noreply.github.com>
2021-09-17 15:51:54 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -4,22 +4,34 @@ import seaborn as sns
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 import matplotlib.pyplot as plt
 import calendar
-df = pd.read_csv('USA_KS_Topeka-Forbes.AFB.724565_TMY3_BASE.csv', usecols = ['Electricity:Facility [kW](Hourly)'])
-dti = pd.date_range("2022-01-01", periods=8760, freq="H")
-df['Datetime'] = dti
-df.index = df['Datetime']
+import datetime
 
 
-df['month'] = pd.to_datetime(df.index).month
-df['month'] = df['month'].apply(lambda x: calendar.month_abbr[x])
-df['hour'] = pd.to_datetime(df.index).hour
+def plot(filename, column, output_fig, x_axis, y_axis, year):
+    df = pd.read_csv(filename, usecols = column)
+    dti = pd.date_range(pd.to_datetime(datetime.datetime(year,1,1)), periods=8760, freq="H")
+    df['Datetime'] = dti
+    df.index = df['Datetime']
+    df['month'] = pd.to_datetime(df.index).month
+    df['month'] = df['month'].apply(lambda x: calendar.month_abbr[x])
+    df['hour'] = pd.to_datetime(df.index).hour
+    
+    sns.lineplot(x=x_axis,
+                 y=y_axis,
+                 hue='month',
+                 data=df,
+                 style = 'month',
+                 markers=True,
+                 sizes = [13,8],
+                 legend = 'auto')
+    plt.savefig(output_fig, dpi=300)
 
-sns.lineplot(x='hour',
-             y='Electricity:Facility [kW](Hourly)',
-             hue='month',
-             data=df,
-             style = 'month',
-             markers=True,
-             sizes = [13,8],
-             legend = 'auto')
-plt.savefig('seaborn-plot.png', dpi=300)
+
+if __name__ == "__main__":
+    filename = 'USA_KS_Topeka-Forbes.AFB.724565_TMY3_BASE.csv'
+    column = ['Electricity:Facility [kW](Hourly)']
+    output_fig = 'seaborn-plot.png'
+    x_axis = 'hour'
+    y_axis = column[0]
+    year = 2022
+    plot(filename, column, output_fig, x_axis, y_axis, year)
