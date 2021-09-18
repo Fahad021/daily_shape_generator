89993db5dab1ba6aee51f2ba43ce4d99e8467452
AHF <47339067+Fahad021@users.noreply.github.com>
2021-09-18 11:55:35 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,15 +1,22 @@
 import pandas as pd
-import statsmodels.api as sm
 import seaborn as sns
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 import matplotlib.pyplot as plt
 import calendar
 import datetime
+from openpyxl import load_workbook
 
-
-def plot(filename, column, output_fig, x_axis, y_axis, year):
-    df = pd.read_csv(filename, usecols = column, nrows = 8760)
-    dti = pd.date_range(pd.to_datetime(datetime.datetime(year,1,1)), periods=8760, freq="H")
+def plot(filename, column, output_fig, x_axis, y_axis, year, timezone):
+    if filename.endswith('.csv'):
+        df = pd.read_csv(filename, usecols = column, nrows=8760)
+    if filename.endswith('.xlsx'):
+        wb = load_workbook(filename)
+        sheet = wb.active
+        all_data = []
+        for i in range (2,8762):
+            all_data.append(sheet["A"+str(i)].value)
+        df = pd.DataFrame(all_data,columns = column)
+    dti = pd.date_range(pd.to_datetime(datetime.datetime(year,1,1)), periods=8760, freq="H", tz = timezone)
     df['Datetime'] = dti
     df.index = df['Datetime']
     df['month'] = pd.to_datetime(df.index).month
@@ -24,15 +31,16 @@ def plot(filename, column, output_fig, x_axis, y_axis, year):
                  markers=True,
                  sizes = [13,8],
                  legend = 'auto',
-                 ci = None)
+                 ci= None)
     plt.savefig(output_fig, dpi=300)
 
 
 if __name__ == "__main__":
-    filename = 'XXXXXX.csv'
-    column = ['XXXXX']
+    filename = 'SPP_North.xlsx'
+    column = ['SPP North Hub']
     output_fig = 'seaborn-plot.png'
     x_axis = 'hour'
     y_axis = column[0]
     year = 2022
-    plot(filename, column, output_fig, x_axis, y_axis, year)
+    timezone = 'US/Central'
+    plot(filename, column, output_fig, x_axis, y_axis, year, timezone)
